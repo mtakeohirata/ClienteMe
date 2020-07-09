3bf85a9a2266989ad85127bcbb83bb79c47e437c
Franklin Hirata <franklinhirata@Franklins-MacBook-Pro.local>
2020-07-09 20:26:12 -0300

    primeiro commit

---
+++ src/main/java/br/com/takeo/projeto/Vendas/App.java
@@ -0,0 +1,13 @@
+package br.com.takeo.projeto.Vendas;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
