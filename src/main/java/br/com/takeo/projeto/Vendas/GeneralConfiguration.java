75cb66748f566ffa8f37722158e798f57c07460e
Franklin Hirata <franklinhirata@Franklins-MacBook-Pro.local>
2020-07-09 22:34:04 -0300

    Ajustes conf Projeto

---
+++ src/main/java/br/com/takeo/projeto/Vendas/GeneralConfiguration.java
@@ -0,0 +1,9 @@
+package br.com.takeo.projeto.Vendas;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class GeneralConfiguration {
+	
+}
